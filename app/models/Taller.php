3d0b236ab37cde78908b02bc0fa9fb082b6ce454
LeonelGonzalezSolis <lgonzalez70733@ufide.ac.cr>
2026-04-06 23:50:32 -0600

    Update Taller.php

--- app/models/Taller.php
+++ app/models/Taller.php
@@ -1,7 +1,6 @@
 <?php
 class Taller
 {
-
     private $conn;
 
     public function __construct($db)
@@ -9,6 +8,7 @@ class Taller
         $this->conn = $db;
     }
 
+    // Listar todos los talleres
     public function getAll()
     {
         $result = $this->conn->query("SELECT * FROM talleres ORDER BY nombre");
@@ -19,23 +19,33 @@ class Taller
         return $talleres;
     }
 
+    // Listar talleres con cupos disponibles
     public function getAllDisponibles()
     {
-
+        $result = $this->conn->query("SELECT * FROM talleres WHERE cupo_disponible > 0 ORDER BY nombre");
+        $talleres = [];
+        while ($row = $result->fetch_assoc()) {
+            $talleres[] = $row;
+        }
+        return $talleres;
     }
 
+    // Buscar taller por ID
     public function getById($id)
     {
-    
+        $stmt = $this->conn->prepare("SELECT * FROM talleres WHERE id=?");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_assoc();
     }
 
+    // Descontar cupo
     public function descontarCupo($tallerId)
     {
-
+        $stmt = $this->conn->prepare("UPDATE talleres SET cupo_disponible=cupo_disponible-1 WHERE id=? AND cupo_disponible>0");
+        $stmt->bind_param("i", $tallerId);
+        return $stmt->execute();
     }
 
-    public function sumarCupo($tallerId)
-    {
-
-    }
 }
